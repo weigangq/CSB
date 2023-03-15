3e87a08641c77c45d8bc1726db3cf247f8ed6859
Weigang Qiu <wgqiu@github.com>
2023-03-15 09:59:11 -0400

    regex update

---
+++ unix/solutions/extract_body_mass.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#######################
+# function of script:
+# take a CSV file delimited by ";" (first argument)
+# remove the header
+# make tab separated
+# sort according to the 6th (numeric) column
+# in descending order
+# redirect to a file (second argument)
+
+# Author: Weigang Qiu
+# Date: Jan 30, 2023
+#########################
+
+# input file name as the 1st argument
+in_file=$1 
+
+# output filename as the 2nd argument
+out_file=$2
+
+# remove the header
+tail -n +2 $1 > $1.tmp1
+
+# extract columns
+cut -d ";" -f 2-6 $1.tmp1 > $1.tmp2
+
+# make tab separated
+tr ";" "\t" < $1.tmp2 > $1.tmp3
+
+# sort and redirect to output
+sort -r -n -k 6 $1.tmp3 > $2
+
+# remove temporary, intermediate files
+rm $1.tmp*
+
+exit;
